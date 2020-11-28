890bfb3eeaa2992933b31d193772c11209138eb6
Felipe Derkian <felipekian@yahoo.com.br>
2020-11-28 14:02:52 -0400

    add teste de retorno dos tokens

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,37 @@
 #include "errors.h"
 #include "lexico.h"
 
-
 using namespace std;
-
 
 int main(int argc, char const *argv[])
 {
     // valida as entradas quanto a quantidade de parametros informados
-    if(argc == 1){error_file_not_information(); exit(EXIT_FAILURE);}
-    if(argc > 2){error_not_extends_params(); exit(EXIT_FAILURE);}
-    if(!setNameFile(argv[argc-1])) exit(EXIT_FAILURE);
+    if (argc == 1)
+    {
+        error_file_not_information();
+        exit(EXIT_FAILURE);
+    }
+    if (argc > 2)
+    {
+        error_not_extends_params();
+        exit(EXIT_FAILURE);
+    }
+    if (!setNameFile(argv[argc - 1]))
+        exit(EXIT_FAILURE);
 
     analisadorLexico();
-    print_lexemas();
+
+
+    cout << "Analise Sintatica" << endl;
+    while (true)
+    {
+        Tokens t = getLexemaSobDemanda();
+
+        if (t.type == -1)
+            break;
+
+        cout << t.getLexema() << "     " << t.getType() << endl;
+    }
 
     return 0;
 }
