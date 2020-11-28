4daf4ad7910e5f420dee270e883f14d860c94981
Felipe Derkian <felipekian@yahoo.com.br>
2020-11-28 15:14:44 -0400

    analisador sintatico LL(1) feito

---
+++ src/sintatico.h
@@ -0,0 +1,52 @@
+/**
+ * 
+ * Analisador Sintático - CompilerExpressions
+ * Autor: Felipe Derkian
+ * Disciplina de Compiladores
+ * Data: 29/11/2020
+ * 
+ */
+
+
+/**
+ * 
+ * Gramática G
+ * 
+ * E  -> T' I (E OP T | T) FE
+ * T' -> id
+ * T  -> id | numero
+ * OP -> + | - | * | /
+ * I  -> :=
+ * FE -> ;
+ * 
+ * 
+ * 
+ * Gramática G' LL(1)
+ * 
+ * 1) E  -> T' I T E' FE
+ * 2) E' -> OP T E' | &
+ * 3) T  -> id | numero
+ * 4) OP -> + | - | * | /
+ * 5) T' -> id
+ * 6) I  -> :=
+ * 7) FE -> ;
+ * 
+ */
+
+
+
+
+void E();       //Inicial
+
+void EL();  //recursao
+
+void T();       //termo | number
+
+void TL();  //termo
+
+void I();       //igual :=
+
+void FE();      //Final Expressão ;
+
+void OP();      //Operador + | - | * | /
+
