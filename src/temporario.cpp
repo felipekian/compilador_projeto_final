a030f6bfca10f4472e612f46e75f0a2144f5c911
Felipe Derkian <felipekian@yahoo.com.br>
2020-11-30 18:55:48 -0400

    gerador de variáveis temporarias para geração de código

---
+++ src/temporario.cpp
@@ -0,0 +1,27 @@
+/**
+ * 
+ * GERA AS VARIÁVEIS TEMPORARIAS PARA GERAÇÂO DE CÓDIGO
+ * Autor: Felipe Derkian
+ * Data: 30/11/2020
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include "temporario.h"
+
+using namespace std;
+
+int contTemp = 0;
+char temp[10];
+char str_int[10];
+
+
+char *getNextTemp()
+{
+    strcpy(temp, "_T");
+    sprintf(str_int, "%d", contTemp);
+    strcat(temp, str_int);
+    contTemp++; 
+    return temp;
+}
---
+++ src/temporario.h
@@ -0,0 +1,9 @@
+/**
+ * 
+ * GERA AS VARIÁVEIS TEMPORARIAS PARA GERAÇÂO DE CÓDIGO
+ * Autor: Felipe Derkian
+ * Data: 30/11/2020
+ * 
+ */
+
+char *getNextTemp();
